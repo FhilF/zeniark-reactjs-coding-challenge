7a52010c88fbe74f52e51ca464b4949be69efa97
FhilF <52390799+FhilF@users.noreply.github.com>
2023-03-22 11:08:10 +0800

    added readme for server

--- web-server/index.js
+++ web-server/index.js
@@ -15,7 +15,7 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(
   session({
---
+++ web-server/readme.md
@@ -0,0 +1,5 @@
+# Trivia Challenge Server
+
+### `npm run start`
+
+Starts the server for the exam project. This is required to operate the trivia challenge web-app.
